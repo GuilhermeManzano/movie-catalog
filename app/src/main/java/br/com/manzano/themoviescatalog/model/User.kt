e6a5ba0c3e3199c533f0af511b071faae2e64d61
Guilherme <guilherme.manzano93@gmail.com>
2022-03-30 22:00:13 -0300

    first commit

---
+++ app/src/main/java/br/com/manzano/themoviescatalog/model/User.kt
@@ -0,0 +1,12 @@
+package br.com.manzano.themoviescatalog.model
+
+class User {
+    private val idUsuario: String? = null
+    private val nomeUsuario: String? = null
+    private val sobrenomeUsuario: String? = null
+    private val sexo: String? = null
+    private val dataNascimento: String? = null
+    private val email: String? = null
+    private val senha: String? = null
+    private val alergiasUsuario: ArrayList<String>? = null
+}
---
+++ app/src/main/java/br/com/manzano/themoviescatalog/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.manzano.themoviescatalog.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.manzano.themoviescatalog.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
